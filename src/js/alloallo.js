794d7e7b132def708b253d5db6a44435ca689036
Peter G.A. Barraud <barraud@adobe.com>
2019-03-11 09:13:56 +0530

    first commit

---
+++ src/js/alloallo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const style = {
+    color: "#f27842"
+}
+export const AlloAllo = React.createElement(
+    'h1',
+    {style: style},
+    "I don't know why you say goodbye. I say Hello."
+)
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import {AlloAllo} from './alloallo'
+
+ReactDOM.render(
+    AlloAllo,
+    document.getElementById('app')
+)
